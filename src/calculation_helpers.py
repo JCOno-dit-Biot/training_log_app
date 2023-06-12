f0b393f679e91f7450a180b4a6231a80a593fd44
JCOno-dit-Biot <jc.onoditb@gmail.com>
2023-06-12 08:32:05 -0400

    convert speed to pace and vice versa

--- src/calculation_helpers.py
+++ src/calculation_helpers.py
@@ -1,11 +1,24 @@
 import numpy as np
-
+from datetime import datetime, timedelta
+import constants as c
 
 def calculate_speed_from_pace(pace):
+
+    pace_datetime=datetime.strptime(pace,"%H:%M:%S")
+    time_in_sec=pace_datetime.minute*c.MIN_TO_SEC + pace_datetime.second
+    speed=c.SEC_IN_HOUR/time_in_sec
 
     return speed
 
 
-def calculate_speed_from_pace(speed):
+def calculate_pace_from_speed(speed):
 
-    return pace
+    time_in_sec=c.SEC_IN_HOUR/speed
+
+    pace_minutes=int(time_in_sec // c.MIN_TO_SEC)
+
+    pace_second=int(time_in_sec % c.MIN_TO_SEC)
+
+    pace_timedelta=timedelta(hours=0, minutes=pace_minutes,seconds=pace_second)
+
+    return str(pace_timedelta)
