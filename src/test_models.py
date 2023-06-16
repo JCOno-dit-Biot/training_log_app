03acfdff3d1da66c8596d3b64aa77c9c3ba7b56e
JCOno-dit-Biot <jc.onoditb@gmail.com>
2023-06-16 09:16:32 -0400

    improve testing for edge cases

--- src/test_models.py
+++ src/test_models.py
@@ -1,15 +1,21 @@
-from models import Dog
+import models
 import pytest
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import calculation_helpers as ch
 
 
+@pytest.fixture
+def training_log_entry(Luna):
+    #dog=models.Dog("Luna",datetime(2017,4,18),models.Kennel("Team Running Huskies"))
+    training_entry= models.Training_Log(datetime.now(), Luna,"Canicross",models.Runner("JC",models.Kennel("Team Running Huskies"),\
+                                        "Christie", 2.4, 3, pace="0:03:20"))
 
+    return training_entry
 
 @pytest.fixture
 def Luna():
-    return Dog('Luna',datetime(2017,4,18))
+    return models.Dog('Luna',datetime(2017,4,18),models.Kennel("Team Running Huskies"))
 
 def test_calculate_dog_age(Luna): 
     date='2023/04/18'   
@@ -25,3 +31,18 @@ def test_speed_to_pace():
     speed=18.0
     pace_string=ch.calculate_pace_from_speed(speed)
     assert pace_string == "0:03:20"
+
+def test_weather_raises_ValError_if_not_in_list():
+    with pytest.raises(ValueError):
+        weather=models.Weather(20,77,sky_condition='clear')
+        
+def test_no_speed_pace_raises_ValError():
+    with pytest.raises(ValueError):
+        training_entry= models.Training_Log(datetime.now(), Luna,"Canicross",models.Runner("JC",models.Kennel("Team Running Huskies")),\
+                                        "Christie", 2.4, 3)
+
+def training_log_calculates_speed_automatically(training_log_entry):
+    assert training_log_entry.speed==18.0
+
+
+
