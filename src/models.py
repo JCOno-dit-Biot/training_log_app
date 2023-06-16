7c3ad18f68d479db2219053dca098ea7af0beb04
JCOno-dit-Biot <jc.onoditb@gmail.com>
2023-06-16 09:16:01 -0400

    add kennel class, update classes dependencies

--- src/models.py
+++ src/models.py
@@ -3,12 +3,17 @@ from dateutil.relativedelta import relativedelta
 
 import constants as c
 import calculation_helpers as ch
+class Kennel:
+
+    def __init__(self, name: str):
+        self.name=name
 
 class Dog:
 
-    def __init__ (self, name: str, DOB):
+    def __init__ (self, name: str, DOB: datetime, kennel: Kennel ):
         self.name=name
         self.DOB=DOB.date()
+        self.kennel=kennel
 
     def calculate_dog_age(self, date: str)  -> float:
 
@@ -17,9 +22,12 @@ class Dog:
         return self.age
 
 class Runner:
-    def __init__(self, name:str):
-        self.name=name
 
+    def __init__(self, name: str, kennel: Kennel):
+        self.name=name
+        self.kennel=kennel
+
+#could make use of descriptors for validation? True for most classes below
 class Dog_Weight:
 
     def __init__(self, dog: Dog, date: str, weight: float):
@@ -34,14 +42,14 @@ class Weather:
     def __init__(self, temperature, humidity, sky_condition = None):
         self.temperature = temperature
         self.humidity=humidity/100
-        if not sky_condition:
+        if sky_condition:
             if sky_condition in c.SKY_CONDITION_LIST:
                 self.sky_condition=sky_condition
             else:
                 raise ValueError(f'The sky condition must be one of the following: {c.SKY_CONDITION_LIST}')
 
 class Training_Log:
-
+    #assumes that pace has the following format "00:00:00" --> need data validation
     def __init__(self, timestamps, dog1:Dog, sport, runner:Runner, location, distance, rating, speed=None, pace=None, dog2= None, workout = False ) -> None:
         self.date=timestamps.date()
         self.time=timestamps.time()
@@ -55,14 +63,18 @@ class Training_Log:
 
         if speed == None and pace == None:
             raise ValueError ("One of {speed} or {pace} must be entered for the training")
+
+        #TODO: if the conversion is only useful in the Training_Log, the functin could be methods
         elif speed == None:
             self.pace=pace
             self.speed=ch.calculate_speed_from_pace(self.pace)
+
         else:
             self.speed=speed
             self.pace=ch.calculate_pace_from_speed(self.speed)
 
         if not dog2:
+            #this is useful if training dogs as a team
             self.dog2=dog2
         
         
