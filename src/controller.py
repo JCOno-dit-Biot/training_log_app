3caac9ffd3c30d2a8358fc6e49231d5a9630132d
JCOno-dit-Biot <jc.onoditb@gmail.com>
2023-07-02 16:03:21 -0400

    add controller, no code specific to sql

---
+++ src/controller.py
@@ -0,0 +1,19 @@
+import src.repository as repository
+from . import models
+
+class Controller:
+
+    def __init__(self, repo: repository.abstract_repository):
+        self.repo=repo
+
+    def get_dog (self, dog_name: str):
+        self.repo.get_dog(dog_name)
+    
+    def get_runner (self, runner_name):
+        self.repo.get_runner(runner_name)
+    
+    def add_weight_entry(self, dog_weigth_entry: models.Dog_Weight):
+        self.repo.add_weight_entry(dog_weigth_entry)
+    
+    def add_training_entry(self, training_entry: models.Training_Log):
+        self.repo.add_training_entry(training_entry)
