e9ada4f6c0725b975172dd519d42d29e3c7e7076
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-04-07 17:09:24 -0400

    add cors middleware for development

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from .utils import get_connection
 from src.api.dog_controller import router as dog_router
@@ -15,6 +16,20 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+# Allow requests from frontend dev server
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,           # frontend URLs allowed
+    allow_credentials=True,
+    allow_methods=["*"],             # allow all HTTP methods
+    allow_headers=["*"],             # allow all headers
+)
 app.include_router(dog_router, tags=["Dogs"])
 app.include_router(runner_router, tags=["Runners"])
 app.include_router(activity_router, tags=["Activities"])
