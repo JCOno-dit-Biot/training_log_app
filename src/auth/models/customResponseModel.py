2355392b6d0b0abc4976d712ed020b26a64cbe38
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-04-25 15:03:22 -0400

    models for HTTP reponse and exceptions

---
+++ src/auth/models/customResponseModel.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import Optional, Any
+
+
+class CustomResponseModel (BaseModel):
+    status_code: int
+    message: Optional[str] = None 
+    data: Optional [Any] =  None # main payload
+    errors: Optional[Any] = None
+
+class SessionTokenResponse(BaseModel):
+    access_token: Optional[str] = None # JWT token
+    token_type: str = "bearer"
+    expires_in: Optional[int] = 3600
+    refresh_token: Optional[str] = None
+
+    
