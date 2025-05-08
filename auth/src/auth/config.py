9e83484244ed1426f4dfef4c43e73ef4de86a02f
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-05-08 16:08:01 -0400

    fix deprecated config pydantic class

--- auth/src/auth/config.py
+++ auth/src/auth/config.py
@@ -1,12 +1,11 @@
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 
 class Settings(BaseSettings):
     ENV: str = "dev"
     DATABASE_URL: str = ""
     TEST_DATABASE_URL: str = ""
 
-    class Config:
-        env_file = "./.env"
-        extra = "ignore"
+    model_config = ConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
