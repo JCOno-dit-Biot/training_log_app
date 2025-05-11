6c3f764c67076b99875104fc2b3313e634d0c7c4
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-05-11 14:06:06 -0400

    Fix: CORS + env var

--- src/config.py
+++ src/config.py
@@ -1,10 +1,13 @@
 from pydantic_settings import BaseSettings
 from pydantic import ConfigDict
+
 class Settings(BaseSettings):
     ENV: str = "dev"
     DATABASE_URL: str = ""
     TEST_DATABASE_URL: str = ""
-
+    AUTH_SERVICE_URL: str = "http://fastapi-auth:8001"
+    ALGORITHM: str = ""
+    SECRET_KEY: str = ""
     model_config = ConfigDict(env_file="config/.env", extra="ignore")
         
 settings = Settings()
