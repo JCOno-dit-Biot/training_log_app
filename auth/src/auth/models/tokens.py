a90e10499cae876ed65524297850c20ae0ff3780
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-05-13 16:27:10 -0400

    refresh token should not be returned in plain text

--- auth/src/auth/models/tokens.py
+++ auth/src/auth/models/tokens.py
@@ -5,4 +5,4 @@ class sessionTokenResponse(BaseModel):
     access_token = Optional[str] = None
     token_type: str = "bearer"
     expires_in = Optional[int] = 3600
-    refresh_token: Optional[str] = None
+    # refresh_token: Optional[str] = None
