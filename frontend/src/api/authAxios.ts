3296967180f9a2c74cb9ba816f78ceb7df9e40ca
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-05-18 18:02:50 -0400

    refactor api folder

--- frontend/src/api/authAxios.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8001',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
-  withCredentials: true
-});
