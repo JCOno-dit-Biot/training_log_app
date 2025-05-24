26ea8568c27ce0536601cda5e0d22c5014db57ee
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-05-24 15:29:20 -0400

    add auth prefix to url

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     ENV: str = "dev"
     DATABASE_URL: str = ""
     TEST_DATABASE_URL: str = ""
-    AUTH_SERVICE_URL: str = "http://fastapi-auth:8001"
+    AUTH_SERVICE_URL: str = "http://fastapi-auth:8001/auth"
     ALGORITHM: str = ""
     SECRET_KEY: str = ""
     model_config = ConfigDict(env_file="config/.env", extra="ignore")
