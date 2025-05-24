2280091634cd1964e7c1eec4145237d0db6ab51e
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-05-24 18:32:12 -0400

    require at least temperature or humitidy for weather

--- src/models/weather.py
+++ src/models/weather.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 from typing import Optional
 
 class Weather(BaseModel):
@@ -6,3 +6,9 @@ class Weather(BaseModel):
     temperature: Optional[float] = Field(None) # temperature in celcius
     humidity: Optional[float] = Field(None, ge =0, le=1)
     condition: Optional[str] = Field(None)
+
+    @model_validator(mode="after")
+    def ensure_at_least_one_metric(cls, values):
+        if values.temperature is None and values.humidity is None:
+            raise ValueError(f"At least one of 'humidity' or 'temperature' must be provided the activity")
+        return values
