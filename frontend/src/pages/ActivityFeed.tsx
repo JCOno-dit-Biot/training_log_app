70f566f8751ed9751d666161ea0d749822047161
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-06-30 11:55:53 -0400

    implement filtering for activities

--- frontend/src/pages/ActivityFeed.tsx
+++ frontend/src/pages/ActivityFeed.tsx
@@ -2,24 +2,29 @@ import { useEffect, useState } from 'react';
 import { getActivities, deleteActivity } from '../api/activities';
 import ActivityCard from '../components/ActivityCard';
 import { Activity } from '../types/Activity';
+import { ActivityFilter } from '../types/ActivityFilter';
 import AddActivityButton from "../components/AddActivityButton";
 import AddActivityForm from "../components/AddActivityForm";
 import { useGlobalCache } from '../context/GlobalCacheContext';
+import { FunnelIcon } from 'lucide-react';
+import ActivityFilterPanel from '../components/ActivityFilterPanel';
 
 export default function ActivityFeed() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [showModal, setShowModal] = useState(false);
+  const [showFilters, setShowFilters] = useState(false);
+  const [filters, setFilters] = useState<ActivityFilter>({})
   const [editActivity, setEditActivity] = useState<Activity | null>(null);
 
   const openEditModal = (activity: Activity) => {
     setEditActivity(activity);
     setShowModal(true);
   };
-  
-  const { sports } = useGlobalCache();
+
+  const { sports, runners, dogs } = useGlobalCache();
 
   useEffect(() => {
-    getActivities( {sports, limit: 10, offset: 0, filters: {}} )
+    getActivities({ sports, limit: 10, offset: 0, filters: {} })
       .then((data) => {
         setActivities(data);
       })
@@ -27,53 +32,78 @@ export default function ActivityFeed() {
   }, []);
 
   const reloadActivities = async () => {
-    const data = await getActivities({sports, limit: 10, offset: 0, filters: {}});
+    const data = await getActivities({ sports, limit: 10, offset: 0, filters: {} });
     setActivities(data);
   };
 
-  const handleDelete = async(activity_id: number) => {
+  const applyFilters = async () => {
+    const data = await getActivities({ sports, filters, limit: 10, offset: 0 });
+    setActivities(data);
+  };
 
-    try{
+  const handleDelete = async (activity_id: number) => {
+
+    try {
       const res = await deleteActivity(activity_id);
 
       if (res.success) {
         setActivities(prev => prev.filter(a => a.id !== activity_id));
       }
-        } catch (err) {
-        console.error('Failed to delete activity', err);
-      }
+    } catch (err) {
+      console.error('Failed to delete activity', err);
     }
-  
+  }
+
 
   return (
     <section className="space-y-4 relative">
-      <h2 className="text-xl font-semibold text-charcoal">Recent Activity</h2>
+      <div className='relative flex justify-center items-center py-2'>
+        <h2 className="flex text-xl text-center font-semibold text-charcoal">Recent Activity</h2>
+      <button
+        onClick={() => setShowFilters(prev => !prev)}
+        className="absolute right-0 flex items-center gap-2 px-4 py-2 border rounded hover:bg-gray-100 sm:px-8"
+      >
+        <FunnelIcon className="w-4 h-4" />
+        Filter
+      </button>
+      </div>
+
+    {showFilters && <ActivityFilterPanel
+      filters={filters}
+      setFilters={setFilters}
+      runners={runners}
+      dogs={dogs}
+      sports={sports}
+      onApply={applyFilters}
+      onClear={() => setFilters({})}
+    />
+    }
       {activities.map((activity) => (
         <ActivityCard
           key={activity.id}
-          activity={activity} 
-          onDelete={handleDelete} 
+          activity={activity}
+          onDelete={handleDelete}
           onSuccess={reloadActivities}
-          onEdit={openEditModal}/>
+          onEdit={openEditModal} />
       ))}
-      
+
       <AddActivityButton onClick={() => setShowModal(true)} />
 
       {showModal && (
         <div className="fixed inset-0 bg-primary/80 flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded-md max-w-xl max-h-[95vh] w-full overflow-y-auto p-6 relative">
             <button
-              onClick={() => {setShowModal(false); setEditActivity(null);}}
+              onClick={() => { setShowModal(false); setEditActivity(null); }}
               className="absolute top-2 right-2 text-gray-600 hover:text-black"
             >
               ✖
             </button>
-            <AddActivityForm 
+            <AddActivityForm
               initialData={editActivity}
               onSuccess={reloadActivities}
               onClose={() => {
-              setEditActivity(null);
-              setShowModal(false);
+                setEditActivity(null);
+                setShowModal(false);
               }} />
           </div>
         </div>
