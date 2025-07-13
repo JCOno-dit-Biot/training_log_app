0bb1e2fbb5f03fd7c194f01c28ff43714ba2947e
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-07-13 17:38:53 -0400

    fix api calls to backend

--- frontend/src/api/stats/weeklyStats.ts
+++ frontend/src/api/stats/weeklyStats.ts
@@ -3,6 +3,6 @@ import { WeeklyStats } from '../../types/WeeklyStats';
 
 
 export const getWeeklyStats = async (): Promise<WeeklyStats[]> => {
-  const res = await axios.get(`/analytics/weekly-stats`);
+  const res = await axios.get(`/analytic/weekly-stats`);
   return res.data;
 };
