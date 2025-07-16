57dff2c9b3c43031476947780dcdfba40d09b958
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-07-16 16:32:06 -0400

    temporary dict for dogcolors

--- frontend/src/components/stats_sidebar/RightSideBar.tsx
+++ frontend/src/components/stats_sidebar/RightSideBar.tsx
@@ -11,10 +11,17 @@ export function RightSidebar(
     weeklyStats: WeeklyStats[],
     dogs: Map<number, Dog>}) {
     
+  // temporary map for development
+  const dogColors = new Map<number, string>([
+    [1, "bg-red-500"],
+    [2, "bg-cyan-400"],
+    [3, "bg-blue-700"],
+    [4, "bg-orange-500"]
+  ])
   console.log(weeklyStats)
   return (
     <div className="fixed right-0 top-0 h-screen w-[350px] border-l bg-primary p-4 shadow-md overflow-y-auto">
-      <StatsCalendar data={calendarData} />
+      <StatsCalendar data={calendarData} dogColors={dogColors} />
       {weeklyStats.map((stat) => {
         const dog = dogs.get(stat.dog_id)
         return dog ? (
