caa05d46a81b953e442c2436aadb518521c496d7
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-07-19 16:30:50 -0400

    modify weekly stat to include selected date as a query param

--- frontend/src/api/stats/weeklyStats.ts
+++ frontend/src/api/stats/weeklyStats.ts
@@ -2,7 +2,7 @@ import axios from '../axios';
 import { WeeklyStats } from '../../types/WeeklyStats';
 
 
-export const getWeeklyStats = async (): Promise<WeeklyStats[]> => {
-  const res = await axios.get(`/analytic/weekly-stats`);
+export const getWeeklyStats = async (date: Date): Promise<WeeklyStats[]> => {
+  const res = await axios.get(`/analytic/weekly-stats?date=${date.toISOString()}`);
   return res.data;
-};
+};  
