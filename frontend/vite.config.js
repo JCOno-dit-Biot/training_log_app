0b4f875221c5b33644d8700e87161df44114101a
JCOno-dit-Biot <jc.onoditb@gmail.com>
2025-12-15 16:22:52 -0500

    build(vite-config): modify config files for using shadecn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
 import path from 'path';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
       '@app': path.resolve(__dirname, 'src/app'),
       '@shared': path.resolve(__dirname, 'src/shared'),
       '@entities': path.resolve(__dirname, 'src/entities'),
