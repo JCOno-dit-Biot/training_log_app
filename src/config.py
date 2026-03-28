6ec97f8d960e37b8f41409d22cdfd9129ed92b1b
JCOno-dit-Biot <jc.onoditb@gmail.com>
2026-03-28 15:53:13 -0400

    fix(backend): allow settings to be None for tests

--- src/config.py
+++ src/config.py
@@ -7,7 +7,10 @@ if os.getenv("ENV") != "test":
     load_dotenv(dotenv_path='config/.env')
     
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(extra="ignore")
+    model_config = SettingsConfigDict(
+        extra="ignore",
+          env_ignore_empty=True
+    )
 
     ENV: str = "dev"
     DATABASE_URL: str = ""
@@ -17,10 +20,10 @@ class Settings(BaseSettings):
     SECRET_KEY: str = ""
     
     #AWS configs
-    AWS_REGION: str
-    AWS_S3_BUCKET_NAME: str
-    AWS_S3_PUBLIC_BASE_URL: str
-    AWS_ACCESS_KEY_ID: str
-    AWS_SECRET_ACCESS_KEY: str
+    AWS_REGION: str = ""
+    AWS_S3_BUCKET_NAME: str = ""
+    AWS_S3_PUBLIC_BASE_URL: str = ""
+    AWS_ACCESS_KEY_ID: str = ""
+    AWS_SECRET_ACCESS_KEY: str = ""
         
 settings = Settings()
